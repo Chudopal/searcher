dea155ffe3f890b0c81c551f8eb0a7ec10723d09
Alexandr Chudopal <alex.ch@core-tech.ru>
2021-11-22 02:41:56 +0300

    added creation for assiotiation

--- app/domain/actions/search_action.py
+++ app/domain/actions/search_action.py
@@ -1,3 +1,6 @@
+import pymorphy2
+from typing import List
+
 from domain.interfaces import ActionInterface
 from domain.interfaces import DatabaseManagerInterface
 
@@ -9,12 +12,30 @@ class SearchAction(ActionInterface):
         database_manager: DatabaseManagerInterface,
         request: str
     ):
-        super().__init__(database_manager=database_manager)
+        super().__init__(
+            database_manager=database_manager
+        )
         self.request = request
+        self.words: List[str]
+        self.word_vector: List[float]
 
     def execute(self):
-        print(self.request)
-        return [
-            "https://www.tutorialspoint.com/python/tk_colors.htm",
-            "https://github.com/github/gitignore/blob/master/Python.gitignore"
+        self.prepare_request()
+        self.create_vectors()
+        self.calculate_similarity()
+        return self.documents = [
+            "rfrfwrf",
+            "wferferfer"
         ]
+
+    def prepare_request(self):
+        morph = pymorphy2.MorphAnalyzer()
+        words = self._prepare_data.split()
+        self.words = [
+            morph.parse(token)[0].normal_form
+            for token in words
+        ]
+        print(self.words)
+
+    def create_vactors(self):
+        pass
